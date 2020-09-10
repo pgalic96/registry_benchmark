ae6dfa9e7c26ae05df310beb8b40cfcbb80b6378
pgalic96 <p.galic96@gmail.com>
2020-09-10 10:22:30 +0200

    Refactor push bench

--- cmd/push.go
+++ cmd/push.go
@@ -3,19 +3,14 @@ package cmd
 import (
 	"bytes"
 	"encoding/csv"
-	"encoding/hex"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
 
-	// Blind
-	"crypto/rand"
-	_ "crypto/sha256"
-	"io/ioutil"
 	"log"
 	"time"
 
-	influxclient "github.com/influxdata/influxdb1-client/v2"
 	"github.com/opencontainers/go-digest"
 	"github.com/pgalic96/docker-registry-client/registry"
 	"github.com/spf13/cobra"
@@ -24,16 +19,7 @@ import (
 	"registry_benchmark/imggen"
 )
 
-func randomHex(n int) (string, error) {
-	bytes := make([]byte, n)
-	if _, err := rand.Read(bytes); err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(bytes), nil
-}
-
 func init() {
-	pullCmd.Flags().BoolVarP(&writeToCSV, "csv", "c", false, "write to local csv file")
 	rootCmd.AddCommand(pushCmd)
 }
 
@@ -42,31 +28,12 @@ var pushCmd = &cobra.Command{
 	Short: "Benchmark docker push with http",
 	Long:  `push generates images and measures push latency`,
 	Run: func(cmd *cobra.Command, args []string) {
-		config, err := loadConfig()
-
-		log.Printf("Configuring influx client")
-		c, err := influxclient.NewHTTPClient(influxclient.HTTPConfig{
-			Addr: config.StorageURL,
-		})
-		if err != nil {
-			log.Fatalf("Error creating InfluxDB Client: ", err.Error())
-		}
-		defer c.Close()
+		config, _ := loadConfig()
 
 		var benchmarkData = make([][]string, len(config.Registries)*config.ImageGeneration.LayerNumber+1)
-		dt := time.Now()
-		csvFile, err := os.Create("push-" + strconv.Itoa(config.ImageGeneration.ImgSizeMb) + "-" + strconv.Itoa(config.ImageGeneration.LayerNumber) + "-" + dt.String() + ".csv")
-		if err != nil {
-			log.Fatalf("failed creating file: %s", err)
-		}
-		var csvwriter = csv.NewWriter(csvFile)
-		defer csvFile.Close()
-		if writeToCSV == true {
-			benchmarkData[0] = []string{"platform", "layer", "latency"}
-		}
+		benchmarkData[0] = []string{"platform", "layer", "latency"}
 
-		log.Printf("Client configured")
-		imggen.Generate()
+		filepath := imggen.Generate()
 		for x, containerReg := range config.Registries {
 			var password string
 			if containerReg.Platform == "ecr" && containerReg.Password == "" {
@@ -83,11 +50,11 @@ var pushCmd = &cobra.Command{
 			if err != nil {
 				log.Fatalf("Error initializing a registry client: %v", err)
 			}
-			for i := 0; i < config.ImageGeneration.LayerNumber; i++ {
-				hexval, _ := randomHex(32)
+			items, _ := ioutil.ReadDir(filepath)
+			for i, item := range items {
 				digest := digest.NewDigestFromHex(
 					"sha256",
-					hexval,
+					item.Name(),
 				)
 				log.Printf("Checking for blob in repository")
 				exists, err := hub.HasBlob(containerReg.Repository, digest)
@@ -96,21 +63,26 @@ var pushCmd = &cobra.Command{
 				}
 				if !exists {
 					log.Printf("Blob not found")
-					file, _ := ioutil.ReadFile("docker-layer-" + strconv.Itoa(i))
+					file, _ := ioutil.ReadFile(filepath + item.Name())
 					start := time.Now()
 					hub.UploadBlob(containerReg.Repository, digest, bytes.NewReader(file), nil)
 					elapsed := time.Since(start)
 					log.Printf("Blob uploaded successfully: %v", elapsed)
-					if writeToCSV == true {
-						benchmarkData[1+i+x*config.ImageGeneration.LayerNumber] = []string{containerReg.Platform, strconv.Itoa(i), elapsed.String()}
-					}
+					benchmarkData[1+i+x*config.ImageGeneration.LayerNumber] = []string{containerReg.Platform, strconv.Itoa(i), elapsed.String()}
 				}
 			}
 		}
 		if writeToCSV == true {
+			dt := time.Now()
+			csvFile, err := os.Create("push-" + strconv.Itoa(config.ImageGeneration.ImgSizeMb) + "-" + strconv.Itoa(config.ImageGeneration.LayerNumber) + "-" + dt.String() + ".csv")
+			if err != nil {
+				log.Fatalf("failed creating file: %s", err)
+			}
+			var csvwriter = csv.NewWriter(csvFile)
 			for _, row := range benchmarkData {
 				csvwriter.Write(row)
 			}
+			csvwriter.Flush()
+			csvFile.Close()
 		}
-		csvwriter.Flush()
 	}}
