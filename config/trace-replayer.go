388010950c0eab2c0799c4ab05259ea206b3e372
pgalic96 <p.galic96@gmail.com>
2020-09-28 17:37:56 +0200

    Add trace-replay config

---
+++ config/trace-replayer.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"io/ioutil"
+)
+
+// TraceReplayerConfig contains necessary auth info for trace replayer
+type TraceReplayerConfig struct {
+	Username   string
+	Password   string
+	Repository string
+}
+
+// SetTraceReplayerEnvVariables sets env variables for the trace replayer to authenticate with registry
+func SetTraceReplayerEnvVariables(filepath string, traceReplayerConfig TraceReplayerConfig) error {
+	envVariables := []byte("REGISTRY_USERNAME=" + traceReplayerConfig.Username + "\nREGISTRY_PASSWORD=" + traceReplayerConfig.Password + "\nREGISTRY_REPO=" + traceReplayerConfig.Repository + "\n")
+	err := ioutil.WriteFile(filepath+"/.env", envVariables, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
